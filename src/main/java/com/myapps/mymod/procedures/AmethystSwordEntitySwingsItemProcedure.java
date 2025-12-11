bfed1a14be7be60bbba41f6e2b1fb48ed6b74fe0
zayoon0o0 <zainn.abdaqqa@gmail.com>
2025-12-11 11:27:48 +0300

    comit

--- src/main/java/com/myapps/mymod/procedures/AmethystSwordEntitySwingsItemProcedure.java
+++ src/main/java/com/myapps/mymod/procedures/AmethystSwordEntitySwingsItemProcedure.java
@@ -1,19 +1,16 @@
 package com.myapps.mymod.procedures;
 
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
 
 public class AmethystSwordEntitySwingsItemProcedure {
-	public static boolean eventResult = true;
-
-	public static void execute(Entity entity) {
-		if (entity == null)
+	public static void execute(Entity target, Entity attacker) {
+		if (target == null)
 			return;
-		if ((entity instanceof Mob _mobEnt ? (Entity) _mobEnt.getTarget() : null) instanceof LivingEntity _entity && !_entity.level().isClientSide()) {
-			_entity.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, 104000, 1));
+		if (target instanceof LivingEntity living && !living.level().isClientSide()) {
+			living.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY, 200, 1));
 		}
 	}
 }
